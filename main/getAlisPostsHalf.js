a70c42c2d2c366533f20dc86c69240e919e84b7b
やよ <omim4841@gmail.com>
2020-06-07 00:15:26 +0900

    ALIS投稿数、いいねの半期ごとの取得

---
+++ main/getAlisPostsHalf.js
@@ -0,0 +1,131 @@
+const fetch = require("node-fetch");
+const { DynamoDB } = require("aws-sdk");
+const dynamodb = new DynamoDB({ region: "ap-northeast-1" });
+const dynClient = new DynamoDB.DocumentClient({
+  endpoint: "http://localhost:8000",
+  service: dynamodb,
+});
+
+async function getUserId() {
+  let users = [];
+  const params = {
+    TableName: "Member",
+  };
+  await dynClient
+    .scan(params, (error, data) => {
+      if (error) console.error(error);
+      else {
+        data.Items.forEach((user) => {
+          users.push({ user_id: user.userId, alis_id: user.alis.id });
+        });
+      }
+    })
+    .promise();
+  return users;
+}
+
+async function getArticlesId(user, date) {
+  const response = await fetch(
+    `https://alis.to/api/users/${user.alis_id}/articles/public`
+  );
+  const body = await response.json();
+  let alis = {
+    id: user.user_id,
+    articles: [],
+  };
+  if ("Items" in body) {
+    body.Items.forEach((article) => {
+      if (article.created_at * 1000 > date.getTime()) {
+        alis.articles.push(article.article_id);
+      }
+    });
+  }
+  return alis;
+}
+
+async function getAlisLikes(alis_data) {
+  let alis = {
+    id: alis_data.id,
+    likes: 0,
+    posts: alis_data.articles.length,
+  };
+  await Promise.all(
+    alis_data.articles.map(async (article) => {
+      const response = await fetch(
+        `https://alis.to/api/articles/${article}/likes`
+      );
+      return await response.json();
+    })
+  )
+    .then((body) => {
+      body.forEach((obj) => {
+        alis.likes += obj.count;
+      });
+    })
+    .catch((err) => {
+      console.log("getAlisLikes");
+      console.log(err);
+    });
+  return alis;
+}
+
+function updateAlisData(users) {
+  users.forEach((user) => {
+    console.log(user.likes, user.posts);
+    const params = {
+      TableName: "Member",
+      Key: {
+        userId: user.id,
+      },
+      UpdateExpression: "SET #a.#l = :likesCount, #a.#p = :postsCount",
+      ExpressionAttributeNames: {
+        "#a": "alis",
+        "#l": "likes_half",
+        "#p": "posts_half",
+      },
+      ExpressionAttributeValues: {
+        ":likesCount": user.likes,
+        ":postsCount": user.posts,
+      },
+    };
+    try {
+      dynClient.update(params).promise();
+    } catch (error) {
+      console.log("updateAlisData");
+      console.log(error);
+    }
+  });
+}
+
+async function main() {
+  const date = new Date();
+  const month = date.getMonth() + 1;
+  let period;
+  if (month !== 4 && month !== 9) {
+    console.log("学期始めではありません");
+    return;
+  }
+  if (month === 4) {
+    period = 18408600000;
+  } else if (month === 9) {
+    period = 15778800000;
+  }
+  date.setTime(date.getTime() - period);
+  const users = await getUserId();
+  Promise.all(
+    users.map(async (user) => {
+      const alis = await getArticlesId(user, date);
+      return await getAlisLikes(alis);
+    })
+  )
+    .then((data) => {
+      updateAlisData(data);
+      console.log("success");
+    })
+    .catch((err) => {
+      console.log("main");
+      console.log(err);
+    });
+}
+
+main();
