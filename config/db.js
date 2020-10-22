ed71db41bc72800ae23e0a9ed897502eb38892a6
やよ <omim4841@gmail.com>
2020-10-22 17:00:39 +0900

    change uri

--- config/db.js
+++ config/db.js
@@ -5,22 +5,13 @@ const option = {
   useNewUrlParser: true,
 };
 const db = mongoose.connection;
-// const mongo_pass = process.env.MONGO_PASS;
-// const db = process.env.MONGO_DB_NAME;
-// const uri = `mongodb://oyoyoa:${mongo_pass}@cluster0.ktjzw.gcp.mongodb.net/${db}?retryWrites=true&w=majority`;
-// const MongoClient = require("mongodb").MongoClient;
-// const uri =
-//   "mongodb+srv://oyoyoa:<password>@cluster0.ktjzw.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, option);
-// client.connect((err) => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+const mongo_pass = process.env.MONGO_PASS;
+const db_name = process.env.MONGO_DB_NAME;
+const uri = `mongodb+srv://oyoyoa:${mongo_pass}@cluster0.ktjzw.gcp.mongodb.net/${db_name}?retryWrites=true&w=majority`;
 
 module.exports.connectDB = function connectDB() {
-  mongoose.connect("mongodb://localhost/test", option);
-  db.on("error", console.error.bind(console, "connection error:"));
+  mongoose.connect(uri, option).catch((err) => console.log(err.reason));
+  //   db.on("error", console.error.bind(console, "connection error:"));
   db.once("open", () => {
     console.log("connecting... You can cancel from ctrl + c");
   });
