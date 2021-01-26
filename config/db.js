35b3b007975faf8023001ae0ae3c134c3db669ba
test <test@PC-8826.local>
2021-01-27 03:20:05 +0900

    使用するmongoDBClusterの変更

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const option = {
 const db = mongoose.connection;
 const mongo_pass = process.env.MONGO_PASS;
 const db_name = process.env.MONGO_DB_NAME;
-const uri = `mongodb+srv://oyoyoa:${mongo_pass}@cluster0.ktjzw.gcp.mongodb.net/${db_name}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://oyoyoa:${mongo_pass}@testugok.zrxlt.mongodb.net/${db_name}?retryWrites=true&w=majority`;
 
 module.exports.connectDB = function connectDB() {
   mongoose.connect(uri, option).catch((err) => console.log(err.reason));
