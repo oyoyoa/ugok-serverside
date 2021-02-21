c0e99214f3fced759a3dc62933a31f836362ea25
やよ <omim4841@gmail.com>
2021-02-21 20:11:34 +0900

    server に triggerをセット

--- config/server.js
+++ config/server.js
@@ -20,4 +20,23 @@ routes(app); //appにRouteを設定する。
 
 app.listen(port); // appを特定のportでlistenさせる。
 
+const cron = require('node-cron'),
+    tweet = require("../main/commands/twitter/createTweetsJson"),
+    tWeek = require("../main/commands/twitter/getTweetsWeek"),
+    tHalf = require("../main/commands/twitter/getTweetsHalf"),
+    tAll = require("../main/commands/twitter/getTweetsAll"),
+    aWeek = require("../main/commands/alis/getAlisPostsWeek"),
+    aHalf = require("../main/commands/alis/getAlisPostsHalf"),
+    aAll = require("../main/commands/alis/getAlisPosts");
+
+cron.schedule('00 3 * * 3', function(){
+    tweet.createTweetsJson();
+    tWeek.getTweetsWeek();
+    tHalf.getTweetsHalf();
+    tAll.getTweetsAll();
+    aWeek.getAlisWeek();
+    aHalf.getAlisHalf();
+    aAll.getAlisAll();
+    });
+
 console.log("RESTful API server started on: " + port);
