9c8cfb20c4ed23608fef71feb438b8ae5e8bd662
やよ <omim4841@gmail.com>
2021-03-05 16:53:44 +0900

    トリガー削除

--- config/server.js
+++ config/server.js
@@ -20,22 +20,22 @@ routes(app); //appにRouteを設定する。
 
 app.listen(port); // appを特定のportでlistenさせる。
 
-const cron = require('node-cron'),
-  {createTweetsJson} = require("../main/commands/twitter/createTweetsJson"),
-  {getTweetsWeek} = require("../main/commands/twitter/getTweetsWeek"),
-  {getTweetsHalf} = require("../main/commands/twitter/getTweetsHalf"),
-  {getTweetsAll} = require("../main/commands/twitter/getTweetsAll"),
-  {getAlisWeek} = require("../main/commands/alis/getAlisPostsWeek"),
-  {getAlisHalf} = require("../main/commands/alis/getAlisPostsHalf"),
-  {getAlisAll} = require("../main/commands/alis/getAlisPosts");
-cron.schedule('00 3 * * 3', function(){
-  createTweetsJson();
-  getTweetsWeek();
-  getTweetsHalf();
-  getTweetsAll();
-  getAlisWeek();
-  getAlisHalf();
-  getAlisAll();
-});
+// const cron = require('node-cron'),
+//   {createTweetsJson} = require("../main/commands/twitter/createTweetsJson"),
+//   {getTweetsWeek} = require("../main/commands/twitter/getTweetsWeek"),
+//   {getTweetsHalf} = require("../main/commands/twitter/getTweetsHalf"),
+//   {getTweetsAll} = require("../main/commands/twitter/getTweetsAll"),
+//   {getAlisWeek} = require("../main/commands/alis/getAlisPostsWeek"),
+//   {getAlisHalf} = require("../main/commands/alis/getAlisPostsHalf"),
+//   {getAlisAll} = require("../main/commands/alis/getAlisPosts");
+// cron.schedule('00 3 * * 3', function(){
+//   createTweetsJson();
+//   getTweetsWeek();
+//   getTweetsHalf();
+//   getTweetsAll();
+//   getAlisWeek();
+//   getAlisHalf();
+//   getAlisAll();
+// });
 
 console.log("RESTful API server started on: " + port);
